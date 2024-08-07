dbcaa397643730ebaa0957f1b77cf064302f2bbd
Gustav Uwe <gustavuwe.123@gmail.com>
2024-08-06 22:02:50 -0300

    feat: start the development of barber page

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/app/_components/ui/button"
+import { db } from "@/app/_lib/prisma"
+import { ChevronLeftIcon, MenuIcon } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Image
+          alt={barbershop?.name}
+          src={barbershop?.imageUrl}
+          fill
+          className="object-cover"
+        />
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute left-4 top-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute right-4 top-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopPage
